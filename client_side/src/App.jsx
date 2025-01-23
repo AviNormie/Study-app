713ee2dbcb50ebb08e2d3a0d69b938474cdeb097
user.name <avi26022007@gmail.com>
2025-01-23 21:31:40 +0530

    Analytics added

--- client_side/src/App.jsx
+++ client_side/src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import {BrowserRouter,Routes,Route} from 'react-router-dom'
-// import SignUp from './pages/SignUp'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Timer from './components/Timer';
-// import Leaderboard from './components/Leaderboard';
 import SignUp from './pages/SignUp';
 import MainPage from './pages/MainPage';
 import Login from './pages/Login';
+import { Analytics } from "@vercel/analytics/react";
 import Room from './pages/Room';
+
 function App() {
   return (
     <div>
@@ -14,13 +14,15 @@ function App() {
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/timer" element={<Timer />} />
-          <Route path="/signup" element={<SignUp/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/room" element={<Room/>} />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/room" element={<Room />} />
         </Routes>
       </BrowserRouter>
+
+      <Analytics />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
